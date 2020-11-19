e03578cbd06beb4b0074fd0323c430c0d36e97d3
vin0922 <74726571+vin0922@users.noreply.github.com>
2020-11-19 15:17:40 -0500

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,8 +1,8 @@
 import os
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix="!")
-TOKEN = os.getenv("DISCORD_TOKEN")
+bot = commands.Bot(command_prefix=".")
+TOKEN = 'Nzc4OTc4ODY1NTk4NDMxMjMy.X7Z2mg.hN-y5_pcKunR1B10gokftNxY_mY'
 
 @bot.event
 async def on_ready():
